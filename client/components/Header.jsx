692d585bba175eca1131c8151c57d2b8af7cc8ed
Diganta Som <itsdsom111@gmail.com>
2022-07-20 00:41:42 +0530

    Added Header in the client

---
+++ client/components/Header.jsx
@@ -0,0 +1,82 @@
+import Image from 'next/image';
+
+import { HiOutlineSearch, HiOutlineHome } from 'react-icons/hi';
+import { RiFlag2Line } from 'react-icons/ri';
+import { MdOutlineOndemandVideo, MdOutlineExpandMore } from 'react-icons/md';
+import { AiOutlineShop, AiFillMessage, AiFillBell } from 'react-icons/ai';
+import { IoGameControllerOutline } from 'react-icons/io5';
+import { CgMenuGridO } from 'react-icons/cg';
+
+const Header = () => {
+  return (
+    <div className='bg-white flex items-center p-2 shadow-md sticky top-0 z-50 h-16'>
+      {/* Left */}
+      <div className='flex min-w-fit'>
+        <Image
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png'
+          alt='Facebook'
+          width={40}
+          height={40}
+        />
+        <div className='flex items-center space-x-2 px-2 ml-2 rounded-full bg-gray-100 text-gray-500'>
+          <HiOutlineSearch size={20} />
+          <input
+            type='text'
+            placeholder='Search Facebook'
+            className='hidden lg:inline-flex bg-transparent focus:outline-none'
+          />
+        </div>
+      </div>
+      {/* Center */}
+      <div className='flex flex-grow justify-center mx-2'>
+        <div className='flex items-center'>
+          <div className='flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer'>
+            <HiOutlineHome size={25} className='mx-auto' />
+          </div>
+          <div className='flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer'>
+            <RiFlag2Line size={25} className='mx-auto' />
+          </div>
+          <div className='flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer'>
+            <MdOutlineOndemandVideo size={25} className='mx-auto' />
+          </div>
+          <div className='flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer'>
+            <AiOutlineShop size={25} className='mx-auto' />
+          </div>
+          <div className='flex items-center h-14 px-4 md:px-10 rounded-md md:hover:bg-gray-100 cursor-pointer'>
+            <IoGameControllerOutline size={25} className='mx-auto' />
+          </div>
+        </div>
+      </div>
+      {/* Right */}
+      <div className='flex items-center justify-end min-w-fit space-x-2'>
+        <Image
+          src='https://upload.wikimedia.org/wikipedia/commons/thumb/b/b8/2021_Facebook_icon.svg/800px-2021_Facebook_icon.svg.png'
+          alt='Facebook'
+          width={40}
+          height={40}
+        />
+        <p className='hidden xl:inline-flex font-semibold text-sm whitespace-nowrap p-3 max-w-xs'>
+          Diganta
+        </p>
+        <CgMenuGridO
+          size={20}
+          className='hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300'
+        />
+        <AiFillMessage
+          size={20}
+          className='hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300'
+        />
+        <AiFillBell
+          size={20}
+          className='hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300'
+        />
+        <MdOutlineExpandMore
+          size={20}
+          className='hidden lg:inline-flex h-10 w-10 bg-gray-200 text-gray-600 rounded-full p-2 cursor-pointer hover:bg-gray-300'
+        />
+      </div>
+    </div>
+  );
+};
+
+export default Header;
