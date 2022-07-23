e301f3ea051a5a52d957670ed7bf844cbca82ff8
Diganta Som <itsdsom111@gmail.com>
2022-07-23 13:01:24 +0530

    Handling fetch-posts and fixing scrollbar on Feed

--- client/components/Posts.jsx
+++ client/components/Posts.jsx
@@ -1,12 +1,24 @@
+import { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
+import axios from 'axios';
 
 import Post from './Post';
 
-import { selectPost } from '../public/src/features/postSlice';
+import { addAllPost, selectPost } from '../public/src/features/postSlice';
 
 const Posts = () => {
+  const FACEBOOK_CLONE_ENDPOINT = 'http://localhost:8080/api/v1/post';
+
   const posts = useSelector(selectPost);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    const fetchData = async () => {
+      const { data } = await axios.get(FACEBOOK_CLONE_ENDPOINT);
+      dispatch(addAllPost(data));
+    };
+    fetchData();
+  }, []);
 
   return (
     <div>
